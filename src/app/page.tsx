60fba711e260b63125637987d882e2922d088b38
Lizyoko <lizyokogg@gmail.com>
2026-06-06 20:15:40 +0800

    feat(search): scroll-to-message with 2s highlight flash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ArtifactPreviewPanel } from '@/components/artifact-preview-panel'
 import { ChatPanel } from '@/components/chat-panel'
 import { FileExplorerPanel } from '@/components/file-explorer-panel'
+import { MessageHighlightLayer } from '@/components/message-highlight-layer'
 import { SelectionPopover } from '@/components/selection-popover'
 import { Sidebar } from '@/components/sidebar'
 
@@ -12,6 +13,7 @@ export default function Home() {
       <FileExplorerPanel />
       <ArtifactPreviewPanel />
       <SelectionPopover />
+      <MessageHighlightLayer />
     </div>
   )
 }
